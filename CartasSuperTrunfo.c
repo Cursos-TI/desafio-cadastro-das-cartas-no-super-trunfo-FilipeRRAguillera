f197f4425c4bee1d75d40ab79ce224a1fe10ccdc
FilipeRRAguillera <filipi.aguillera@hotmail.com>
2025-02-20 12:23:02 +0000

    V1.0 Cadastramento de cartas / modelo simples

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,22 +1,44 @@
 #include <stdio.h>
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
-// Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
-
 int main() {
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
-    
-    // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
-    
-    // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+
+    // Variaveis da cidade
+    char codi[4], nome[21]; // Código da cidade, Nome da cidade
+    int popu, pont; // A população da cidade, O numero de Pontos Turísticos da cidade
+    float area, pib; // A area em Km² da cidade, PIB da cidade
+
+
+
+    // A fase de cadastramento da cidade
+    printf("Cadastro de cartas do jogo Super Trunfo\n\n");
+
+    printf("Insira o código da cidade.\n");
+    scanf("%s", &codi);
+
+    printf("Insira o nome da cidade.\n");
+    scanf("%s", &nome);
+
+    printf("Insira a população da cidade.\n");
+    scanf("%d", &popu);
+
+    printf("Insira a área em km quadrado da cidade.\n");
+    scanf("%f", &area);
+
+    printf("Insira o PIB da cidade.\n");
+    scanf("%f", &pib);
+
+    printf("Insira a quantidade de pontos turísticos da cidade.\n");
+    scanf("%d", &pont);
+
+
+
+    // A fase de exibição de dados da cidade cadastrada
+    printf("\nO código da cidade: %s\n", codi);
+    printf("O nome da cidade: %s\n", nome);
+    printf("A população da cidade: %d\n", popu);
+    printf("A área da cidade: %.2fKm²\n", area);
+    printf("O PIB da cidade: R$%.2f\n", pib);
+    printf("A quantidade de pontos turísticos da cidade: %d\n", pont);
 
     return 0;
 }
